b36255af35262a65e76371b5870cb1249ea3d585
Phonicia Anne Peramaiyan <153049020+PhoniciaAnne@users.noreply.github.com>
2025-04-11 16:56:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/index.css';  // Our Tailwind & global CSS
+import './styles/index.css';  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
